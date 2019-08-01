f209c93c946c45b18e10bf48ac171c54e2b65c90
Stephen Nam <stnam1994@gmail.com>
2019-08-01 15:05:31 -0400

    Added ImmutableObject class

---
+++ src/day3/ImmutableObject.java
@@ -0,0 +1,25 @@
+package day3;
+
+public class ImmutableObject {
+
+	private String name;
+	private int value;
+	
+	public ImmutableObject(String name, int value) {
+		this.name = name;
+		this.value = value;
+	}
+	
+	@Override
+	public Object clone() throws CloneNotSupportedException {
+	    return super.clone();
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public int value() {
+		return this.value;
+	}
+}
